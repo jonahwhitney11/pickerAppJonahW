a2acefda056edc530d0bb23731b866fac411460c
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-02-21 12:46:51 -0600

    custom picker, Initial commit

--- pickerApp/ViewController.swift
+++ pickerApp/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var myPicker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        myPicker.delegate = self
+        myPicker.dataSource = self
+        pickerData = ["cat", "dog", "mouse", "turtle", "monkey", "bird"]
         // Do any additional setup after loading the view.
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        let pickerIndex = myPicker.selectedRow(inComponent: 0)
+        let alert = UIAlertController(title: "Your choice", message: "\(pickerData[pickerIndex])", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: {action -> Void in
+        })
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
